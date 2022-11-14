42795daee5e4ad8b8be297b88dc9e4ce64e5cc45
zz <206830641@qq.com>
2022-11-15 03:35:27 +0800

    hot-fix commit2

--- src/main/java/com/zz/GitTest.java
+++ src/main/java/com/zz/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git22");
         System.out.println("hello git33");
         System.out.println("hello git44");
+        System.out.println("hot-fix test");
     }
 
 }
